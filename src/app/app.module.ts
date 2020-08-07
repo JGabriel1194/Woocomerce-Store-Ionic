c70e39c2a904567cafcbb423f997700ff8201781
Gabriel Soto <juanga_esp@outlook.es>
2020-08-07 00:03:26 -0500

    se creo la pagina de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginPipe } from './pages/login.pipe';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginPipe],
   entryComponents: [],
   imports: [
     BrowserModule, 
